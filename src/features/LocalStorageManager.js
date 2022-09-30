885879b68439bbb9863b27adf644ae95c92e9caf
Szabolcs David <davidsz@inf.u-szeged.hu>
2022-09-30 15:56:42 +0200

    Start implementation of a local storage manager

---
+++ src/features/LocalStorageManager.js
@@ -0,0 +1,25 @@
+let instance;
+
+class LocalStorageManager {
+    constructor() {
+        if (instance) throw new Error("You can only create one instance of LocalStorageManager.");
+        instance = this;
+
+        // Reserved key for saving current application state
+        this.CURRENT = "current-state";
+    }
+
+    set(key, value) {
+        localStorage.setItem(key, JSON.stringify({
+            date: new Date().getTime(),
+            value: value
+        }));
+    }
+
+    get(key) {
+        return JSON.parse(localStorage.getItem(key));
+    }
+}
+
+const singletonLocalStorageManager = new LocalStorageManager();
+export default singletonLocalStorageManager;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React, { createContext } from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
+import singletonLocalStorageManager from "./features/LocalStorageManager";
 import singletonWorkspaceEngine from "./features/WorkspaceEngine";
 import App from "./App";
 import { theme } from "./theme";
 
+export const LocalStorageContext = createContext();
 export const WorkspaceContext = createContext();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <ThemeProvider theme={theme}>
-        <WorkspaceContext.Provider value={singletonWorkspaceEngine}>
-            <App />
-        </WorkspaceContext.Provider>
+        <LocalStorageContext.Provider value={singletonLocalStorageManager}>
+            <WorkspaceContext.Provider value={singletonWorkspaceEngine}>
+                <App />
+            </WorkspaceContext.Provider>
+        </LocalStorageContext.Provider>
     </ThemeProvider>
 );
